5e2460e4f33695ab687d370f0dabbae23d31a5d3
Pierre Averty <pierre.averty2001@gmail.com>
2024-03-18 14:36:08 +0100

    adding the loading function

--- src/DSIFTDescriptorForObjectDetection.jl
+++ src/DSIFTDescriptorForObjectDetection.jl
@@ -1,5 +1,34 @@
 module DSIFTDescriptorForObjectDetection
 
-greet() = print("Hello World!")
+using FileIO, 
+    Images
+
+"""
+    loadImgByPatches(image::AbstractMatrix, dim::Tuple{Int, Int}=(16, 16), stride::Tuple{Int, Int}=(8,8))
+
+Load an image by dividing it into patches of specified dimensions and stride.
+
+# Arguments
+- `image`: The input image as an AbstractMatrix.
+- `dim`: The dimensions of each patch as a tuple of integers. Default is (16, 16).
+- `stride`: The stride between patches as a tuple of integers. Default is (8, 8).
+
+# Returns
+An array of patches extracted from the image.
+
+"""
+function loadImgByPatches(image::AbstractMatrix, dim::Tuple{Int, Int}=(16, 16), stride::Tuple{Int, Int}=(8,8))
+    patches = []
+
+    for i in 1:stride[1]:size(image, 1)-(dim[1]-1)
+        for j in 1:stride[2]:size(image, 2)-(dim[2]-1)
+            patch = image[i:i+(dim[1]-1), j:j+(dim[2]-1)]
+            push!(patches, patch)
+        end
+    end
+    return patches
+end
 
 end # module DSIFTDescriptorForObjectDetection
+
+using FileIO
